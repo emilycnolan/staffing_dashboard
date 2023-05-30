fa86a265b876a8c9c8c9df48c4ad44ee9fc6b289
emilycnolan <119525860+emilycnolan@users.noreply.github.com>
2023-05-29 20:40:30 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,161 @@
+# Define server logic
+server <- function(input, output) {
+  library(shinydashboard)
+  library(markdown)
+  library(thematic)
+  library(rsconnect)
+  thematic_on()
+  
+  data <- read.csv("StaffingDataSet.csv")
+  data <- data[rowSums(is.na(data))==0,]
+  data$Median.Household.Income <- as.numeric(gsub("\\$","", gsub(",","", 
+                                                                 data$Median.Household.Income)))
+  data$X..change.18.23 <- as.numeric(gsub("%","",data$X..change.18.23))
+  data$X..change.20.23 <- as.numeric(gsub("%","",data$X..change.20.23))
+  
+  output$sum <- renderTable({
+    subset_data <- subset(data, data$X2018.2023 == input$direction)
+    subsum <- as.matrix(summary(subset_data$X..change.18.23))
+    colnames(subsum) <- c("Quantity")
+    subsum
+  }, rownames = TRUE)  
+  
+  output$vars <- renderTable({
+    variables <- matrix(c("Column one contains the name of the school district(character)","Column two contains the name of the town that attends the district(character)", "Column three has the population of the town (integer)", "Column four has the median household income of the town (numeric)", "Column five has the equalized net grand list per capita of the town (numeric)", "Columns six through ten have the number of staff in the district in the 2018-19, 2019-20, 2020-21, 2021-22, and 2022-23 school years (all numeric)", "Columns eleven and twelve both contain strings that either state increased or decreased, this is indicating if the number of staff increased or decreased from 2018 to 2023 and 2020 to 2023 respectively", "Columns thirteen and fourteen have the percent change in the number of staff from 2018-2023 and 2020-2023 respectively (numeric)"),ncol=1, byrow=TRUE)
+    colnames(variables) <- c('Variable Description')
+    rownames(variables) <- c('District Name', 'Town Name', 'Population', 'Median Household Income', 'Equalized Grand Net List Per Capita', 'Number of Staff', 'Increase or Decrease', 'Percent Change')
+    variables
+  }, rownames = TRUE)
+  
+  output$freq <- renderTable({
+    if(input$years == 1){
+      var <- data$X2018.2023
+    } else{
+      var <- data$X2020.2023
+    }
+    frequ <- as.matrix(with(data, table(var)))
+    colnames(frequ) <- c("Number of Districts")
+    frequ
+  }, rownames = TRUE)
+  
+  output$box1 <- renderPlot({
+    boxplot(data$X..change.18.23, data$X..change.20.23, main="Boxplots of Changes in Staffing Levels", 
+            ylab="Percent Change", names=c("2018-2023", "2020-2023"), col="lightskyblue3")
+  })
+  
+  output$hists <- renderPlot({
+    if(input$category == 1){
+      hist(data$Population, main="Histogram of Population", 
+           xlab="Population Size", col="lightskyblue3")
+    } else if(input$category == 2){
+      hist(data$Median.Household.Income, main="Histogram of Median Household Income", 
+           xlab="Median Household Income ($)", col="lightskyblue3")
+    } else{
+      hist(data$Equalized.Net.Grand.List.Per.Capita, main="Histogram of Equalized Net Grand List", 
+           xlab="Equalized Net Grand List per Capita ($)", col="lightskyblue3")
+    }
+  })
+  
+  output$means <- renderPlot({
+    yearsdf <- data.frame(years = c('2018-19', '2019-20', '2020-21','2021-22',
+                                    '2022-23'), means = c(mean(data$X2018.19),
+                                                          mean(data$X2019.20), mean(data$X2020.21),
+                                                          mean(data$X2021.22), mean(data$X2022.23)))
+    plot(yearsdf$means, pch=20, xlab="Years", ylim = c(80, 115),
+         ylab="Average Number of Staff", xaxt="n", main = "Average Number of Staff for Each Year Across All Districts", col="lightskyblue3")
+    axis(1, at=1:5, labels=yearsdf$years)
+    lines(yearsdf$means,xlim=range(yearsdf$means), pch=16)
+    
+  })
+  output$histsizetitle <- renderText({
+    if(input$size == 1){
+      "Number of staff in towns with a population < 7,000 (histograms)"
+    } else if(input$size == 2){
+      "Number of staff in towns with 7,000<Population<20,000 (histograms)"
+    } else{
+      "Number of staff in towns with Population > 20,000 (histograms)"
+    }
+  })
+  
+  output$sizeyearstitle <- renderText({
+    if(input$size == 1){
+      "Number of staff in towns with a population < 7,000 over the years"
+    } else if(input$size == 2){
+      "Number of staff in towns with 7,000<Population<20,000 over the years"
+    } else{
+      "Number of staff in towns with Population > 20,000 over the years"
+    }
+  })
+  
+  output$histsize <- renderPlot({
+    smallpop <- subset(data, data$Population<=7000)
+    largepop <- subset(data, data$Population>=20000)
+    medpop <- subset(data, data$Population>7000 & data$Population<20000)
+    if(input$size == 1){
+      par(mfrow=c(2,3))
+      hist(smallpop$X2018.19, main='2018-2019', col="lightskyblue3")
+      hist(smallpop$X2019.20, main='2019-2020', col="lightskyblue3")
+      hist(smallpop$X2020.21, main='2020-2021', col="lightskyblue3")
+      hist(smallpop$X2021.22, main='2021-2022', col="lightskyblue3")
+      hist(smallpop$X2022.23, main='2022-2023', col="lightskyblue3")
+    } else if(input$size == 2){
+      par(mfrow=c(2,3))
+      hist(medpop$X2018.19, main='2018-2019', col="lightskyblue3")
+      hist(medpop$X2019.20, main='2019-2020', col="lightskyblue3")
+      hist(medpop$X2020.21, main='2020-2021', col="lightskyblue3")
+      hist(medpop$X2021.22, main='2021-2022', col="lightskyblue3")
+      hist(medpop$X2022.23, main='2022-2023', col="lightskyblue3")
+    } else{
+      par(mfrow=c(2,3))
+      hist(largepop$X2018.19, main='2018-2019', col="lightskyblue3")
+      hist(largepop$X2019.20, main='2019-2020', col="lightskyblue3")
+      hist(largepop$X2020.21, main='2020-2021', col="lightskyblue3")
+      hist(largepop$X2021.22, main='2021-2022', col="lightskyblue3")
+      hist(largepop$X2022.23, main='2022-2023', col="lightskyblue3")
+    }
+  })
+  
+  output$yearsbysize <- renderPlot({
+    smallpop <- subset(data, data$Population<=7000)
+    largepop <- subset(data, data$Population>=20000)
+    medpop <- subset(data, data$Population>7000 & data$Population<20000)
+    if(input$size == 1){
+      smallpopavg <- data.frame(years = c('2018-19', '2019-20', '2020-21','2021-22',
+                                          '2022-23'), means = c(mean(smallpop$X2018.19),
+                                                                mean(smallpop$X2019.20), mean(smallpop$X2020.21),
+                                                                mean(smallpop$X2021.22), mean(smallpop$X2022.23)))
+      plot(smallpopavg$means, pch=20, xlab="Years", ylab="Average Number of Staff",
+           xaxt="n", main="Number of staff in small towns over the years", col="lightskyblue3")
+      axis(1, at=1:5, labels=smallpopavg$years)
+      lines(smallpopavg$means,xlim=range(smallpopavg$means), pch=16)
+    } else if(input$size == 2){
+      medpopavg <- data.frame(years = c('2018-19', '2019-20', '2020-21','2021-22',
+                                        '2022-23'), means = c(mean(medpop$X2018.19),
+                                                              mean(medpop$X2019.20), mean(medpop$X2020.21),
+                                                              mean(medpop$X2021.22), mean(medpop$X2022.23)))
+      plot(medpopavg$means, pch=20, xlab="Years", ylab="Average Number of Staff",
+           xaxt="n", main="Number of staff in medium towns over the years", col="lightskyblue3")
+      axis(1, at=1:5, labels=medpopavg$years)
+      lines(medpopavg$means,xlim=range(medpopavg$means), pch=16)
+    } else{
+      largepopavg <- data.frame(years = c('2018-19', '2019-20', '2020-21','2021-22',
+                                          '2022-23'), means = c(mean(largepop$X2018.19),
+                                                                mean(largepop$X2019.20), mean(largepop$X2020.21),
+                                                                mean(largepop$X2021.22), mean(largepop$X2022.23)))
+      plot(largepopavg$means, pch=20, xlab="Years", ylab="Average Number of Staff",
+           xaxt="n", main="Number of staff in large towns over the years", col="lightskyblue3")
+      axis(1, at=1:5, labels=largepopavg$years)
+      lines(largepopavg$means,xlim=range(largepopavg$means), pch=16)
+    }})
+  output$mymap <- renderLeaflet({
+    leaflet(
+      addProviderTiles(providers$Stamen.TonerLite,options = providerTileOptions(noWrap = TRUE)), 
+      addMarkers(data = points()))
+  })
+  
+}
+
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,131 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+#library(shiny)
+library(shinydashboard)
+library(markdown)
+library(thematic)
+library(rsconnect)
+library(leaflet)
+library(packrat)
+
+
+inactivity <- "function idleTimer() {
+  var t = setTimeout(logout, 5000);
+  window.onmousemove = resetTimer; // catches mouse movements
+  window.onmousedown = resetTimer; // catches mouse movements
+  window.onclick = resetTimer;     // catches mouse clicks
+  window.onscroll = resetTimer;    // catches scrolling
+  window.onkeypress = resetTimer;  //catches keyboard actions
+
+  function logout() {
+    window.close();  //close the window
+  }
+
+  function resetTimer() {
+    clearTimeout(t);
+    t = setTimeout(logout, 5000);  // time is in milliseconds (1000 is 1 second)
+  }
+}
+idleTimer();"
+
+rsconnect::setAccountInfo(name='emily-nolan', token='5512FE9BA4A9A1EF27F99C5603CF4AF1', secret='ROzEuf1D6+cJTYXix0R3CsbvCEnzMXxoKIu2pzI/')
+
+data <- read.csv("StaffingDataSet.csv")
+data <- data[rowSums(is.na(data))==0,]
+data$Median.Household.Income <- as.numeric(gsub("\\$","", gsub(",","", 
+                                                               data$Median.Household.Income)))
+data$X..change.18.23 <- as.numeric(gsub("%","",data$X..change.18.23))
+data$X..change.20.23 <- as.numeric(gsub("%","",data$X..change.20.23))
+
+
+ui <- fluidPage(
+  tags$script(inactivity),
+  
+  # Application title
+  navbarPage("Connecticut Staffing Data",
+             theme = shinythemes::shinytheme("darkly"),
+             
+             tabPanel("About", icon = icon("circle-info"),
+                      fluidRow(
+                        column(6,
+                               tags$h2("Objectives"),
+                               tags$body("In this analysis I am looking to study the changes in staffing levels across a 5 year period, 2018
+to 2023. Was there a change in staffing levels in 2020 due to Covid-19? Have staff quantities
+increased or decreased over the past 5 years? What are the staffing trends in the larger cities
+of Connecticut?"),
+                               tags$h2("Methods"),
+                               tags$body("The methods followed in this analysis include cleaning the data, creating some general descriptive statistics, performing a t-test, and creating visualizations."),
+                               tags$h2("Data Description"),
+                               tags$body("The dataset I’m using in this analysis is from the latest report on CT Public School staff from
+EdSight (public-edsight.ct.gov), a government website with data on many different aspects of
+the school system."),
+                               
+                               tags$h2("Interpretations and Conclusions"),
+                               tags$body("From the analysis presented in this report, I can answer my original research questions. The first objective
+was to see if Covid-19, 2020-2021, had an impact on staffing levels. The first indication that Covid-19 did
+have an impact is from the t-test I performed which concluded that the mean change in staff for 2018-2023
+was different from the mean change in staff for 2020-2023. That shows that there is a different in the rate
+of change when the Covid-19 year is included. The next indication that it had an impact comes from the
+visualizations created. The plot showing the average number of staff for each year shows a dip in the quantity
+of staff in the 2020-2021 school year. This may mean that schools got rid of staff for the year Covid-19 had
+it’s biggest impact. I also found that towns with a larger population (greater than 20,000), saw the largest
+dip in staff in the 2020-21 year. This answered my next objective which was to see what the trend was across
+the state and across larger cities specifically. In towns with large populations, cities, the trend continued
+upwards but with the larger dip in 2020. For the state as a whole, staffing levels have been increasing at a
+fairly steady rate over the past 5 years."),
+                               column(width=6, tableOutput("vars"))
+                        ))),
+             navbarMenu("Data Analysis", icon = icon("magnifying-glass-chart"),
+                        tabPanel("Summaries", icon = icon("chart-bar"),
+                                 # Show a plot of the generated distribution
+                                 mainPanel(
+                                   tags$h3("Descriptive Statistics"),
+                                   wellPanel(sidebarLayout(sidebarPanel(checkboxGroupInput("direction",
+                                                                                           "Districts with Staff that:",
+                                                                                           choices = c("increased", "decreased"), 
+                                                                                           selected=NULL)),
+                                                           mainPanel(tableOutput("sum")))),
+                                   tags$h3("Frequency"),
+                                   wellPanel(sidebarLayout(sidebarPanel(radioButtons("years",
+                                                                                     "Districts that increased or decreased from:",
+                                                                                     choices = list("2018 to 2023" = 1, 
+                                                                                                    "2020 to 2023" = 2), 
+                                                                                     selected=NULL)),
+                                                           mainPanel(tableOutput("freq")))),
+                                 )
+                        ),
+                        tabPanel("Visualizations", icon = icon("chart-line"),
+                                 mainPanel(width = 12,
+                                           wellPanel(plotOutput("box1")),
+                                           wellPanel(sidebarLayout(sidebarPanel(radioButtons("category",
+                                                                                             "Histogram of _______ Variable", choices = list(
+                                                                                               "Population" = 1, "Median Household Income" = 2,
+                                                                                               "Equalized Net Grand List Per Capita" = 3), selected = NULL)),
+                                                                   mainPanel(plotOutput("hists")))),
+                                           wellPanel(plotOutput("means"),
+                                                     tags$body("This graph shows the average number of staff for each year across all districts. There has been an
+upward trend over the past 5 years, dipping slightly in the 2020-21 school year. This is another indication
+that Covid-19 may have had an impact on staffing levels.")),
+                                           wellPanel(sidebarLayout(sidebarPanel(radioButtons("size", "Graphs by Population Size",
+                                                                                             list("Less than 7,000" = 1, "7,000 to 20,000" = 2,
+                                                                                                  "Greater than 20,000" = 3), selected = NULL)),
+                                                                   mainPanel(tags$h3(textOutput("histsizetitle")),
+                                                                             plotOutput("histsize"),
+                                                                             tags$h3(textOutput("sizeyearstitle")),
+                                                                             plotOutput("yearsbysize")
+                                                                   )))
+                                 )
+                        )
+             ))
+  #leafletOutput("mymap")
+)
+
+deployApp()
+
